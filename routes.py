578e2cc7a9d60d5b9c8564c5a9886f2492a71caf
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2024-09-28 15:21:58 -0500

    added code for userview

--- routes.py
+++ routes.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, redirect, url_for
 from datetime import datetime
-import sqlite3
+
+from .dataloader import load_data
+
 
 app = Flask(__name__)
+stations = load_data("data.csv")
 
 
 # @app.route("/")
@@ -10,28 +13,18 @@ app = Flask(__name__)
 #     return render_template("landing-page.html", stations=stati)
 
 
-@app.route("/userview/<station>", methods=["GET", "POST"])
-def user_view(station):
-    if request.method == "POST":
-        pass
-    pass
-
-
-# @app.route("/success")
-# def success():
-#     return render_template("success.html")
+@app.route("/userview/", methods=["POST"])
+def user_view():
+    data = request.get_json()
+    start_station = data.get("start")
+    final_station = data.get("final")
+    stations[start_station].needs_ramp = True
 
 
 @app.route("/workerview/<line>/<station>")
 def worker_view(line, station):
+    # TODO return a boolean to indicate whether the worker should place the ramp
     pass
-
-
-@app.template_filter("format_datetime")
-def format_datetime(value, format="%Y-%m-%d %H:%M:%S"):
-    if value is None:
-        return ""
-    return datetime.strptime(value, "%Y-%m-%d %H:%M:%S").strftime(format)
 
 
 if __name__ == "__main__":
